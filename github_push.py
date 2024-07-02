5824cf359ff7770235de6ade5645ef11f4caf565
lucesgabriel <lucesgabriel@gmail.com>
2024-07-02 19:54:28 -0400

    Commit inicial: MercadoLibre Product Tracker

--- github_push.py
+++ github_push.py
@@ -9,13 +9,19 @@ def run_command(command):
         return False
     return True
 
+def get_current_branch():
+    process = subprocess.Popen(["git", "rev-parse", "--abbrev-ref", "HEAD"], stdout=subprocess.PIPE)
+    output, _ = process.communicate()
+    return output.decode('utf-8').strip()
+
 def push_to_github(repo_name, commit_message):
+    current_branch = get_current_branch()
     commands = [
         "git init",
         "git add .",
         f'git commit -m "{commit_message}"',
         f"git remote add origin https://github.com/lucesgabriel/{repo_name}.git",
-        "git push -u origin main"
+        f"git push -u origin {current_branch}:{current_branch}"
     ]
 
     for command in commands:
@@ -27,6 +33,6 @@ def push_to_github(repo_name, commit_message):
     return True
 
 if __name__ == "__main__":
-    repo_name = "mercadolibre-product-tracker"
+    repo_name = "Mercadolibre_api"
     commit_message = "Commit inicial: MercadoLibre Product Tracker"
     push_to_github(repo_name, commit_message)
